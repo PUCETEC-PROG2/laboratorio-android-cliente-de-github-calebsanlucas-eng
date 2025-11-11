f84d77eafb3e523e0bfa0557fb19a90d93ec9a4d
Caleb San Lucas <cssan@puce.edu.ec>
2025-11-11 16:35:00 -0500

    Avance Laboratorio 3 p2

---
+++ app/src/main/java/models/Repo.kt
@@ -0,0 +1,4 @@
+package ec.edu.uisek.githubclient.models
+
+class Repo {
+}
---
+++ app/src/main/java/services/GitHubApiService.kt
@@ -0,0 +1,7 @@
+package ec.edu.uisek.githubclient.services
+
+import retrofi
+
+interface GitHubApiService {
+    @GET(value)
+}
